c6259a3f3ac81eb8e493072307c186d905c16216
Aldeny Junior <aldenyjr.dev@gmail.com>
2023-10-24 12:21:18 -0300

    Mapeamento de herança @Inheritance(strategy = InheritanceType.JOINED)

--- src/main/java/br/com/alura/loja/modelo/Produto.java
+++ src/main/java/br/com/alura/loja/modelo/Produto.java
@@ -9,7 +9,7 @@ import java.time.LocalDate;
 @Table(name = "produtos")
 @NamedQuery(name = "Produto.produtosPorCategoria",
         query = "SELECT p FROM Produto p WHERE p.categoria.nome = ?1")
-@Inheritance(strategy = InheritanceType.SINGLE_TABLE)
+@Inheritance(strategy = InheritanceType.JOINED)
 public class Produto {
 
     @Id
